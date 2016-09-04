d43b71dd954cdbea28e748ca52b843a7f589e29e
jsw324 <jwalkow@tallience.com>
2016-09-03 22:33:53 -0400

    checking config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,6 @@
-//= require welcome
-console.info("APP");
-//= require twitter/bootstrap
-//= require bootstrap/dropdown
-//= require bootstrap
-//= require turbolinks
-//= require_tree
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require bootstrap
+//= require turbolinks
+//= require_tree
