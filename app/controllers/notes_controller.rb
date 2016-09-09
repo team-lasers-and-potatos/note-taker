154a36a611805aa98d74dbaeaee3d8da817a02b4
[jsw324] <jwalkow@tallience.com>
2016-09-09 15:12:21 -0400

    created functional create/show models.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,6 +4,28 @@ class NotesController < ApplicationController
   end
 
   def new
-    @notes = Note.create(title: :title,body: :body)
+    @notes = Note.new
+  end
+
+  def create
+    @notes = Note.new(note_params)
+    if @notes.save && !@notes.nil?
+      redirect_to notes_path
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+    @notes = Note.destroy
+  end
+
+  private
+  def note_params
+   params.require(:note).permit(:title, :body)
   end
 end
